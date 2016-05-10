3a79cb409456a66a13fc0c545940f1ccd9978675
h0rror <spiceace@gmail.com>
2016-05-10 14:16:51 -0400

    Added Test Project

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWordlToConsole();
+        }
+
+        /* This is my hellow World Method
+         * return
+         * */
+        public static string HelloWordlToConsole()
+        {
+            string HelloWorldString = " Hello, World";
+            Console.WriteLine(HelloWorldString);
+                return HelloWorldString;
+
+        }
+    }
+}
